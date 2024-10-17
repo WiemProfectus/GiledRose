022a79b6ef44acc587ac41d4c40b89d48b7bdfc5
WiemProfectus <wiem.zouabi@profectus-it.be>
2024-10-17 15:39:21 +0200

    Add handling for "Conjured" items in updateQuality and update tests

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,4 +14,62 @@ class GildedRoseTest {
         assertEquals("fixme", app.items[0].name);
     }
 
+    @Test
+    void agedBrieIncreasesInQuality() {
+        Item[] items = new Item[] { new Item("Aged Brie", 2, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(1, app.items[0].quality);
+        assertEquals(1, app.items[0].sellIn);
+    }
+
+    @Test
+    void qualityNeverExceeds50() {
+        Item[] items = new Item[] { new Item("Aged Brie", 2, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(50, app.items[0].quality);
+    }
+
+    @Test
+    void sulfurasNeverDecreasesInQuality() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(80, app.items[0].quality);
+        assertEquals(0, app.items[0].sellIn);
+    }
+
+    @Test
+    void backstagePassesIncreaseInQuality() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(21, app.items[0].quality);
+        assertEquals(10, app.items[0].sellIn);
+    }
+
+    @Test
+    void backstagePassesQualityDropsToZeroAfterConcert() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void conjuredItemsDegradeTwiceAsFast() {
+        Item[] items = new Item[] { new Item("Conjured Mana Cake", 3, 6) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(4, app.items[0].quality);
+        assertEquals(2, app.items[0].sellIn);
+    }
+    @Test
+    void expiredItemsDegradeTwiceAsFast() {
+        Item[] items = new Item[] { new Item("foo", 0, 6) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(4, app.items[0].quality);
+    }
 }
